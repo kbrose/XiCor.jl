4770e354a9e351fe0c90ea727065cc83e1906416
Kevin Rose <kevin+gh@maypark.com>
2022-02-02 23:56:14 -0600

    I believe this implementation is correct.

--- src/XiCorr.jl
+++ src/XiCorr.jl
@@ -1,5 +1,41 @@
+using Random
+
 module XiCorr
 
-greet() = print("Hello World!")
+function xicorr(X, Y)
+    n = length(X)
+    Y = Y[sortperm(X)]  # how should offset arrays be handled?
+    sorter = sortperm(Y)
+    R = zeros(Int, n)
+    L = zeros(Int, n)
+
+    i = 1
+    while i <= n
+        curr = Y[sorter[i]]
+        counter = 1
+        i += 1  # look ahead for repeated Y values
+        while i <= n && Y[sorter[i]] == curr
+            counter += 1
+            i += 1
+        end
+        i -= 1
+        for j = i-counter+1:i
+            R[sorter[j]] = i
+            L[sorter[j]] = (n-i) + counter
+        end
+        i += 1
+    end
+
+    1 - n * sum(abs.(diff(R))) / (2 * sum(L .* (n .- L)))
+end
+
+function xicorr(x, y, break_ties_randomly=false)
+    if break_ties_randomly
+        index = randperm(length(x))
+        x = x[index]
+        y = y[index]
+    end
+    xicorr(x, y)
+end
 
 end # module
